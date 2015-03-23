74afd49b390c97a4d3005dc5d1496a0455314d98
kpatrickbarrett <kpatrickbarrett@gmail.com>
2015-03-23 14:22:33 -0400

    Adding from bitbucket
    pages beginning to 595

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card : MonoBehaviour {
+	public string suit;
+	public int rank;
+	public Color color = Color.black;
+	public string colS = "Black";
+
+	public List<GameObject> decoGOs = new List<GameObject>();
+	public List<GameObject> pipGOs = new List<GameObject>();
+
+	public GameObject back; //back of card
+	public CardDefinition def;//parsed from deck
+
+	public bool faceUp{
+		get{return(!back.activeSelf);}
+		set{back.setActive(!value);}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+[System.Serializable]
+public class Decorator{
+	public string type;
+	public Vector3 loc;
+	public bool flip = false;
+	public float scale = 1f;
+}
+
+[System.Serializable]
+public class CardDefinition{
+	public string face;
+	public int rank;
+	public List<Decorator> pips = new List<Decorator>();
+}
---
+++ Assets/Scripts/Prospector.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Prospector : MonoBehaviour {
+	static public Prospector S;
+	public Deck deck;
+	public TextAsset deckXML;
+	public Layout layout;
+	public TextAsset layoutXML;
+	public List<CardProspector> drawpile;
+	public Vector3 layoutCenter; 
+	public float xOffset = 3;
+	public float yOffset = -2.5f;
+	public Transform layoutAnchor;
+	public CardProspector target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
+
+
+
+
+	void Awake(){
+		S = this;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		deck = GetComponent<Deck> ();
+		deck.InitDeck (deckXML.text);
+		layout = GetComponent<Layout>();
+		layout.ReadLayout(layoutXML.text);
+		drawpile = ConvertListCardsToListCardProspectors(deck.cards);
+		LayoutGame();
+
+	}
+
+	CardProspector Draw(){
+		CardProspector cd = drawpile[0];
+		drawpile.RemoveAt(0);
+		return(cd);
+	}
+
+
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD){
+		List<CardProspector> lCP = new List<CardProspector>();
+		CardProspector tCP;
+		foreach(Card tCD in lCD){
+			tCP = tCD as CardProspector;
+			lCP.Add(tCP);
+		}
+		return(lCP);
+	}
+
+	void LayoutGame(){
+		if(layoutAnchor == null){
+			GameObject tGO = new GameObject("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+
+		CardProspector cp;
+		foreach(SlotDef tSD in layout.SlotDefs){
+			cp = Draw();
+			cp.faceUp = tSD.faceUp;
+			cp.transform.parent = layoutAnchor;
+			cp.transform.localPosition = new Vector3(layout.multiplier.x * tSD.x, layout.multiplier.y * tSD.y, -tSD.layerID);
+			cp.layoutID = tSD.id;
+			cp.slotDef = tSD;
+			cp.state = CardState.tableau;
+
+			tableau.Add(cp);
+		}
+	}
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
